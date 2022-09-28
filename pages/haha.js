ab41d6865f73241a003d97abb59b3902bfd6926d
MBbold <mbold81@gmail.com>
2022-09-28 10:44:45 +0800

    button

--- pages/haha.js
+++ pages/haha.js
@@ -1,8 +1,15 @@
+import { useRouter } from "next/router"
 
 const haha = () => {
-  return (
-    <div>haha</div>
-  )
+    const route = useRouter()
+    return (
+        <>
+            <div>haha</div>
+            <button onClick={() => {
+                route.push("/")
+            }}>go to home</button>
+        </>
+    )
 }
 
 export default haha
