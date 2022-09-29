06658157581da56d1a0c21f2829ee57a91690580
MBbold <mbold81@gmail.com>
2022-09-29 09:38:22 +0800

    navbar new

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,23 @@
+import { AppBar, Button, Container, CssBaseline, List, ListItemButton, Toolbar } from '@mui/material'
+import { Box } from '@mui/system'
 import Head from 'next/head'
-import Image from 'next/image'
 import { useRouter } from 'next/router'
 import styles from '../styles/Home.module.css'
+import { Logo } from './icons/logo'
+import { Navbar } from './Navbar'
 
 export default function Home() {
   const router = useRouter()
+  const buttons = ['Products', 'Services', 'Contact', 'Log in'];
   return (
-    <div className={styles.container}>
-      hahaha
-      <button onClick={()=>{
-        router.push('haha')
-      }}>go to haha</button>
-    </div>
+    <>
+      <CssBaseline />
+      <Container sx={{ width: '100vw', height: '100vh', backgroundColor: '#fff' }}>
+        
+        <Navbar />
+
+      </Container>
+    </>
+
   )
 }
